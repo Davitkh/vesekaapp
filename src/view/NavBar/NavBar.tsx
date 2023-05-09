e989088e46574c9fe63d2ec8733ef0cba1528404
Davit <davitkh91@gmail.com>
2023-05-10 00:02:31 +0300

    change readme file

--- src/view/NavBar/NavBar.tsx
+++ src/view/NavBar/NavBar.tsx
@@ -1,11 +1,11 @@
-import { NavbarIcon } from '../../icons/Logo/NavbarIcon';
-import styles from './NavBar.module.css';
-import clx from 'classnames';
-import { NavLink } from 'react-router-dom';
+import { NavbarIcon } from "../../icons/Logo/NavbarIcon";
+import styles from "./NavBar.module.css";
+import clx from "classnames";
+import { NavLink } from "react-router-dom";
 
 export function NavBar() {
   return (
-    <div className={clx(styles.navbar, 'navbar_area')}>
+    <div className={clx(styles.navbar, "navbar_area")}>
       {/*TODO: read css-flex csstrics*/}
       <div className={clx(styles.navbar_logo)}>
         <NavbarIcon />
