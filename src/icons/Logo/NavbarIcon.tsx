41dc54d59449190c20c84497d5159e4ade3b370c
Edgar <ed.hovhannis1992@gmail.com>
2023-05-09 12:11:56 +0300

    add favicon, form, auth state

--- src/icons/Logo/NavbarIcon.tsx
+++ src/icons/Logo/NavbarIcon.tsx
@@ -1,8 +1,9 @@
 import styles from './NavbarIcon.module.css';
-
+import { useHistory } from 'react-router-dom';
 export const NavbarIcon = () => {
+  const history = useHistory();
   return (
-    <div className={styles.navbaricon}>
+    <div className={styles.navbaricon} onClick={() => history.push('/')}>
       <div className={styles.navbaricon_left}></div>
       <div className={styles.navbaricon_middle}></div>
       <div className={styles.navbaricon_right}></div>
