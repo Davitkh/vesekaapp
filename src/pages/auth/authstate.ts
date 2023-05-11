62137f26a0f2c3a615aef9a1a39178e34d400e41
Edgar <ed.hovhannis1992@gmail.com>
2023-05-11 20:30:52 +0300

    fix double render bug, refactor authstate

--- src/pages/auth/authstate.ts
+++ src/pages/auth/authstate.ts
@@ -1,61 +1,139 @@
-import { SyntheticEvent } from "react";
-import { ElementTypeTitles } from "../../enums/element_type_titles";
+import { SyntheticEvent } from 'react';
+import { ElementTypeTitles } from '../../enums/element_type_titles';
 
-interface LoginTypes {
+interface AuthItemTypes {
   label: string;
   type: ElementTypeTitles;
   name: string;
   placeholder: string;
-  onClick?: (e: SyntheticEvent) => void;
+  onClick?: (e?: SyntheticEvent, setter?: Function) => void;
 }
 
-interface LoginStateType {
-  login: LoginTypes;
+export enum AuthStateKeys {
+  Login = 'login',
+  Register = 'register',
+  Restore = 'restore',
 }
 
-export const LoginState = (setter: Function): LoginStateType[] => [
-  {
-    login: {
-      label: "Логин",
-      type: ElementTypeTitles.Email,
-      name: "login",
-      placeholder: "телефон или почту",
+type AuthStateTypes = {
+  [key in AuthStateKeys]?: AuthItemTypes[];
+};
+
+export const handleAuthState = (authtype: AuthStateKeys, setter: Function) => {
+  const AuthState: AuthStateTypes[] = [
+    {
+      login: [
+        {
+          label: 'Логин',
+          type: ElementTypeTitles.Email,
+          name: 'login',
+          placeholder: 'телефон или почту',
+        },
+        {
+          label: 'Пароль',
+          type: ElementTypeTitles.Password,
+          name: 'login',
+          placeholder: 'пароль',
+        },
+        {
+          label: 'Забыли пароль ?',
+          type: ElementTypeTitles.Span,
+          name: '',
+          placeholder: '',
+        },
+        {
+          label: 'Войти',
+          type: ElementTypeTitles.Submit,
+          name: '',
+          placeholder: '',
+        },
+        {
+          label: 'Создать аккаунт',
+          type: ElementTypeTitles.Button,
+          name: '',
+          placeholder: '',
+          onClick: (e) => {
+            e && e.preventDefault();
+            setter && setter('register');
+          },
+        },
+      ],
     },
-  },
-  {
-    login: {
-      label: "Пароль",
-      type: ElementTypeTitles.Password,
-      name: "login",
-      placeholder: "пароль",
+    {
+      register: [
+        {
+          label: 'Логин',
+          type: ElementTypeTitles.Email,
+          name: 'login',
+          placeholder: 'телефон или почту',
+        },
+        {
+          label: 'Имя',
+          type: ElementTypeTitles.Email,
+          name: 'login',
+          placeholder: 'имя пользователя',
+        },
+        {
+          label: 'Пароль',
+          type: ElementTypeTitles.Password,
+          name: 'password',
+          placeholder: 'пароль',
+        },
+        {
+          label: 'Повтарите пароль',
+          type: ElementTypeTitles.Password,
+          name: 'reapetpassword',
+          placeholder: 'повторите пароль',
+        },
+        {
+          label: 'Зарегистрироваться',
+          type: ElementTypeTitles.Button,
+          name: '',
+          placeholder: '',
+        },
+      ],
     },
-  },
-  {
-    login: {
-      label: "Забыли пароль ?",
-      type: ElementTypeTitles.Span,
-      name: "",
-      placeholder: "",
+    {
+      restore: [
+        {
+          label: 'Логин',
+          type: ElementTypeTitles.Email,
+          name: 'login',
+          placeholder: 'телефон или почту',
+        },
+        {
+          label: 'Пароль',
+          type: ElementTypeTitles.Password,
+          name: 'login',
+          placeholder: 'пароль',
+        },
+        {
+          label: 'Забыли пароль ?',
+          type: ElementTypeTitles.Span,
+          name: '',
+          placeholder: '',
+        },
+        {
+          label: 'Войти',
+          type: ElementTypeTitles.Submit,
+          name: '',
+          placeholder: '',
+        },
+        {
+          label: 'Зарегистрироваться',
+          type: ElementTypeTitles.Button,
+          name: '',
+          placeholder: '',
+          // onClick: (e, setter) => {
+          //   e && e.preventDefault();
+          //   setter && setter('register');
+          // },
+        },
+      ],
     },
-  },
-  {
-    login: {
-      label: "Войти",
-      type: ElementTypeTitles.Submit,
-      name: "",
-      placeholder: "",
-    },
-  },
-  {
-    login: {
-      label: "Зарегистрироваться",
-      type: ElementTypeTitles.Button,
-      name: "",
-      placeholder: "",
-      onClick: (e) => {
-        e.preventDefault();
-        setter("register");
-      },
-    },
-  },
-];
+  ];
+  const needData = AuthState.find((data) => data[authtype]);
+  if (needData) {
+    return needData[authtype];
+  }
+};
--- src/view/Content/Content.tsx
+++ src/view/Content/Content.tsx
@@ -1,16 +1,16 @@
-import clx from "classnames";
-import styles from "./Content.module.css";
-import { Route } from "react-router-dom";
-import { Maps } from "../../pages/maps/maps";
-import { Home } from "../../pages/home/home";
-import { Auth } from "../../pages/auth/auth";
+import clx from 'classnames';
+import styles from './Content.module.css';
+import { Route } from 'react-router-dom';
+import { Maps } from '../../pages/maps/maps';
+import { Home } from '../../pages/home/home';
+import { Auth } from '../../pages/auth/auth';
 
 export const Content = () => {
   return (
-    <div className={clx(styles.content, "content_area")}>
-      <Route exact path={"/"} component={Home} />
-      <Route path={"/maps"} component={Maps} />
-      <Route path={"/auth"} component={Auth} />
+    <div className={clx(styles.content, 'content_area')}>
+      <Route exact path={'/'} component={Home} />
+      <Route path={'/maps'} component={Maps} />
+      <Route path={'/auth'} component={Auth} />
     </div>
   );
 };
