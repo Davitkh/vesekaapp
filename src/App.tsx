722ec9bcd1300f22345aaf1e00c7c8094f7693c0
Edgar <ed.hovhannis1992@gmail.com>
2023-07-21 22:10:07 +0300

    fix conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-import React from "react";
-import { NavBar } from "./view/NavBar/NavBar";
-import { Content } from "./view/Content/Content";
+import { NavBar } from './view/NavBar/NavBar';
+import { Content } from './view/Content/Content';
 
 function App() {
   return (
