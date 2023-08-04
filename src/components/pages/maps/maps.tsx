177dd8a6ba02580b8f37b7d80fb3fe8c211122fc
Эдгар Оганнисян <ESOgannisyan@sberbank.ru>
2023-08-04 22:07:27 +0300

    Add new routing and new navbar

---
+++ src/components/pages/maps/maps.tsx
@@ -0,0 +1,8 @@
+import { useEffect } from "react";
+
+export const Maps = () => {
+  useEffect(() => {
+    document.title = "Maps";
+  });
+  return <div>Maps</div>;
+};
--- src/components/partials/navbar/navbar.tsx
+++ src/components/partials/navbar/navbar.tsx
@@ -1,6 +1,18 @@
 import cls from "classnames";
 import styles from "./navbar.module.css";
+import { NavLink } from "react-router-dom";
 
 export const Navbar = () => {
-  return <div className={cls(styles.navbar_)}>navbar</div>;
+  return (
+    <div className={cls(styles.navbar_)}>
+      <div className={cls(styles.navbar_logo)}>
+        <span>V</span>eselka
+      </div>
+      <div className={cls(styles.navbar_items)}>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/maps">Maps</NavLink>
+        <NavLink to="/auth">Auth</NavLink>
+      </div>
+    </div>
+  );
 };
